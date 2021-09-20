664c325b4299863ffb3a0beb85d98e4cda33f090
Liubov <luba_siagaeva@mail.ru>
2021-09-20 18:23:38 +0300

    added media

---
+++ js/burger.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.menu-burger').click(function() {
+        $('.menu-burger').toggleClass('open-menu');
+        $('.top-menu').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+    });
+});
+
